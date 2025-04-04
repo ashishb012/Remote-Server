30d539db737d768f4f14821f891e91fce64cf948
ashishb012 <ashishb0128@gmail.com>
2025-04-04 16:35:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route to receive and display data
+app.post('/data', (req, res) => {
+    const receivedData = req.body;
+    console.log('Received data:', receivedData);
+    res.json({
+        message: 'Data received successfully',
+        data: receivedData
+    });
+});
+
+// Route to get data
+app.get('/data', (req, res) => {
+    res.send('Welcome to the data endpoint! Send a POST request to submit data.');
+});
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Server is running! Use /data endpoint to interact.');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
